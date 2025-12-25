0016086f43fe6cd125cbe9ec53f5692f59082e10
Tetyana Popenko <orkneya17@gmail.com>
2025-12-25 23:55:22 +0200

    all in conteiner

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
+import Container from "../Container/Container";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <span className={styles.logoPrimary}>Nani</span>
-        <span className={styles.logoSecondary}>Services</span>
-      </div>
+      <Container className={styles.headerInner}>
+        <div className={styles.logo}>
+          <span className={styles.logoPrimary}>Nani</span>
+          <span className={styles.logoSecondary}>Services</span>
+        </div>
 
-      <nav className={styles.nav}>
-        <NavLink to="/" className={styles.link}>
-          Home
-        </NavLink>
-        <NavLink to="/nannies" className={styles.link}>
-          Nannies
-        </NavLink>
-      </nav>
+        <nav className={styles.nav}>
+          <NavLink to="/" className={styles.link}>
+            Home
+          </NavLink>
+          <NavLink to="/nannies" className={styles.link}>
+            Nannies
+          </NavLink>
+        </nav>
 
-      <div className={styles.auth}>
-        <button className={styles.authBtn}>Login</button>
-        <button className={styles.authBtn}>Registration</button>
-      </div>
+        <div className={styles.auth}>
+          <button className={styles.authBtn}>Login</button>
+          <button className={styles.authBtn}>Registration</button>
+        </div>
+      </Container>
     </header>
   );
 }
