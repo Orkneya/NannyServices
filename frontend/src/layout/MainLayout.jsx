9ad33fe51b671d821d88b4b501c9a61994f2fb0e
Tetyana Popenko <orkneya17@gmail.com>
2025-12-25 20:06:42 +0200

    version for mobile, tebles container

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header.jsx";
+import Container from "../components/Container/Container.jsx";
 
 export default function MainLayout() {
   return (
     <>
       <Header />
       <main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
     </>
   );
