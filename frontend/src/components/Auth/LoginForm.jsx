4ce2e6de144f8abe77db5fad0c663262a0be6779
Tetyana Popenko <orkneya17@gmail.com>
2026-01-05 22:52:41 +0200

    With Validation

--- frontend/src/components/Auth/LoginForm.jsx
+++ frontend/src/components/Auth/LoginForm.jsx
@@ -1,19 +1,48 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { loginSchema } from "../../schemas/authSchema.js";
+
 import styles from "./Auth.module.css";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("Login data:", data);
+    // тут позже будет Firebase login
+  };
   return (
-    <div className={styles.login}>
+    <form className={styles.login} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={styles.title}>Log In</h2>
       <p className={styles.subtitle}>
-        Welcome back! Please enter your details.
+        Welcome back! Please enter your credenials to access your account and
+        continue your babbysitter search .
       </p>
 
       <div className={styles.fields}>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        {/* <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" /> */}
+        <input type="email" placeholder="Email" {...register("email")} />
+        {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className={styles.error}>{errors.password.message}</p>
+        )}
       </div>
 
       <button className={styles.submitBtn}>Login</button>
-    </div>
+    </form>
   );
 }
--- frontend/src/components/Auth/RegisterForm.jsx
+++ frontend/src/components/Auth/RegisterForm.jsx
@@ -1,20 +1,52 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { registerSchema } from "../../schemas/authSchema.js";
+
 import styles from "./Auth.module.css";
 
 export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("Register data:", data);
+    // тут позже будет Firebase registr
+  };
   return (
-    <div className={styles.register}>
+    <form className={styles.register} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={styles.title}>Registration</h2>
       <p className={styles.subtitle}>
-        Thank you for registration! Please fill in the fields below.
+        Thank you for your interest in our platform! In order to register, we
+        need some information.Please provide us with the followihg information
       </p>
 
       <div className={styles.fields}>
-        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Name" {...register("name")} />
+        {errors.name && <p className={styles.error}>{errors.name.message}</p>}
+
+        <input type="email" placeholder="Email" {...register("email")} />
+        {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className={styles.error}>{errors.password.message}</p>
+        )}
+        {/* <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Password" /> */}
       </div>
 
       <button className={styles.submitBtn}>Sign Up</button>
-    </div>
+    </form>
   );
 }
