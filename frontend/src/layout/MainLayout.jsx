934906a4a352ab668da0818860cd6a5b81c5273f
Tetyana Popenko <orkneya17@gmail.com>
2026-01-08 14:09:08 +0200

    add scroll

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header.jsx";
 import Container from "../components/Container/Container.jsx";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop.jsx";
 
 export default function MainLayout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main>
         <Container>
